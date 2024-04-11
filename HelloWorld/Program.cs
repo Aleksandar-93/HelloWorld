8d362447c2cba6a8e449cb7c896b2e56b11903c7
Aleksandar <60047712+Aleksandar-93@users.noreply.github.com>
2024-04-11 23:00:04 +0200

    Parsing a string to intager

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,31 +6,18 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            //Implicit and explicit Conversion
+            //Parsing a string to intager
 
-            // Implicit Conversion
-            int num = 12390532;
-            long bigNum = num;
+            string myString = "15";
+            string MySecondString = "13";
+            int num1 = Int32.Parse(myString);
+            int num2 = Int32.Parse(MySecondString);
+            int resultInt = num1 + num2;
+            string result = myString + MySecondString; 
 
-            float myFloat = 13.37f;
-            double myNewDouble = myFloat;
-
-
-            //explicit Conversion
-            double myDouble = 13.37;
-            int myInt;
-            //cast double to int 
-            myInt = (int)myDouble; 
-
-            //typeConversion 
-            string myString = myDouble.ToString(); //"13.37"
-            string myFloatString = myFloat.ToString();
-            bool sunIsShining = false;
-
-            string myBooulString = sunIsShining.ToString();
-
-            Console.WriteLine(myBooulString);
+            Console.WriteLine(resultInt);
             Console.Read();
+
         }
     }
 }
