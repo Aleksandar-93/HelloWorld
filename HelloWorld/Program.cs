f826663020715a3f11fd5576184d56125a72bab0
Aleksandar <60047712+Aleksandar-93@users.noreply.github.com>
2024-04-11 23:49:16 +0200

    String Manipulation

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,18 +6,45 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            //Parsing a string to intager
+            //String Manipulation
+
+            int age = 28;
+            string name = "Alfonso";
+            string job = "Devoloper";
+
+            // 1. String concatenation 
+            Console.WriteLine("String concatienation");
+            Console.WriteLine("Hello my name is " + name + " , I am " + age + " years old");
+
+            // 2. String formating 
+            // String format use index
+            Console.WriteLine("String formating");
+            Console.WriteLine("Hello my name is {0} , I am {1} years old. I'm a {2}",name,age,job);
+
+            // 3. String interpolation 
+            // String interpolation use $ at the start wich will allow us to write wariables like this {name}
+            Console.WriteLine("String interpolation");
+            Console.WriteLine($"Hello my name is {name} , I am {age} years old.", name, age);
+
+            // 4. verbatim Strings
+            // verbatim Strings start with @ and tell compiler to take a string
+            //literally and ignore any spaces and escape chiracter like \n
+            Console.WriteLine("verbatim Strings");
+            Console.WriteLine(@"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
+
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore 
+            eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
 
 
-            string stringForFloat = "0.85"; // datatype should be float
-            string stringForInt = "12345"; // datatype should be int
+            culpa qui officia deserunt mollit anim id est 
+            laborum.");
+            //good for 
+            Console.WriteLine(@"c:\User\Admin\Pictures\.....");
 
-            float stringToFloat = float.Parse(stringForFloat);
-            int stringToInt = int.Parse(stringForInt);
 
-
-            Console.WriteLine(stringForInt);
-            Console.Read();
+            // \n next line (line brake ) but @ ignore line brake 
+            Console.WriteLine(@"Muhaha \n you dont have power here!.");
+            Console.WriteLine("Muhaha \n you dont have power here!.");
 
         }
     }
