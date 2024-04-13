20e12f16951500902c1396014d5fb21fe801a9c1
Aleksandar <60047712+Aleksandar-93@users.noreply.github.com>
2024-04-13 15:46:19 +0200

    constants

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,20 +1,27 @@
 ﻿using System;
 
-namespace StringPratice
+namespace ConstantsC
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //Var data type
+            // Constants are immutable values which are known 
+            // at compile time and do not change for the life of the program.
 
-            var number = 0; // must bee initaliezed 
-            //number = "sometext"; Need to be same type
-            number = 2;
-            // var num3; Implicitly-typed variables must be initialized
-            var text = "text";
-            var isTrue = false; //boolean
-            //number = null; var canot bee null 
+            // constants as fields
+            const double PI = 3.14159265359;
+            const int weeks = 52, months = 12;
+            // Create a constant of type string with your birthday as its value
+            const string birthday = "07.03.1993";
+            const string birthday2 = "05.31.1988";
+            const string birthday3 = "2018-05-31";
+
+            static void Main(string[] args)
+            {
+                Console.WriteLine("My birthday is always going to be: {0}", birthday);
+                Console.ReadKey();
+            }
         }
     }
 }
