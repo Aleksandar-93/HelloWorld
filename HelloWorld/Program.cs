e757b4d78cf5441347c54a69af3e3d5937a5c797
Aleksandar <60047712+Aleksandar-93@users.noreply.github.com>
2024-04-13 16:47:17 +0200

    void Method

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,27 +1,25 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
 
-namespace ConstantsC
+namespace functionMethods
 {
     class Program
     {
         static void Main(string[] args)
         {
-            // Constants are immutable values which are known 
-            // at compile time and do not change for the life of the program.
+            WriteSomting();
+            WriteSomtingSpecific("I am a argument and i am called from a method");
+            Console.ReadLine();
+            
+        }
 
-            // constants as fields
-            const double PI = 3.14159265359;
-            const int weeks = 52, months = 12;
-            // Create a constant of type string with your birthday as its value
-            const string birthday = "07.03.1993";
-            const string birthday2 = "05.31.1988";
-            const string birthday3 = "2018-05-31";
-
-            static void Main(string[] args)
-            {
-                Console.WriteLine("My birthday is always going to be: {0}", birthday);
-                Console.ReadKey();
-            }
+        public static void WriteSomting()
+        {
+            Console.WriteLine("I am called from a method");
+        }
+        public static void WriteSomtingSpecific(string myText)
+        {
+            Console.WriteLine(myText);
         }
     }
 }
