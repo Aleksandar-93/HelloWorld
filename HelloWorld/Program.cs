925953499d09b0a879c52a314b9180be9dbf673d
Aleksandar <60047712+Aleksandar-93@users.noreply.github.com>
2024-04-14 13:06:29 +0200

    Try Catch finaly

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -2,37 +2,58 @@
 using System.ComponentModel;
 using System.Security.Cryptography.X509Certificates;
 
-namespace UsserInput
+namespace TryCatch
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Calculate();
-            Console.Read();
+
+            Console.WriteLine("Please enter a number!");
+            string userInput = Console.ReadLine();
+
+            int num1 = 5;
+            int num2 = 0;
+            int result;
+
+           
+          
+            try
+            {
+               result = num1 / num2;
+
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("cant devide by Zeroo!");
+            }
+
+
+
+
+            try
+            {
+                int userInputAsInt = int.Parse(userInput);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Format expection, please neter corect type next time.");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Overflow Exception, the number ws to long or to shgort for int32.");
+            }
+            finally
+            {
+                Console.WriteLine("Thiss is called anyweys!");
+            } 
+
+
+
+            Console.ReadKey();
         }
 
-        public static void Calculate() 
-        {
-
-            Console.WriteLine("Plis insert first nummber");
-            string input1 = Console.ReadLine();
-            Console.WriteLine("Plis insert second nummber");
-            string input2 = Console.ReadLine();
-
-            double addTwoString = double.Parse(input1) + double.Parse(input2);
-            double subTwoString = double.Parse(input1) - double.Parse(input2);
-            double mulTwoString = double.Parse(input1) * double.Parse(input2);
-            double divTwoString = double.Parse(input1) / double.Parse(input2);
-
-            Console.WriteLine("First number + second number is " + addTwoString);
-            Console.WriteLine("First number - second number is " + subTwoString);
-            Console.WriteLine("First number * second number is " + mulTwoString);
-            Console.WriteLine("First number / second number is " + divTwoString);
-
-
-            
-        }
+       
 
 
     }
