9743768425ef5adcb51f7c6247afc79e9863b0b2
Aleksandar <60047712+Aleksandar-93@users.noreply.github.com>
2024-04-15 23:43:12 +0200

    Registering And Loggin In

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,37 +1,53 @@
 ﻿using System;
 using System.ComponentModel;
+using System.Reflection.Metadata;
 using System.Security.Cryptography.X509Certificates;
 
-namespace ifPratice
+namespace RegisteringAndLogginIn
 {
-    class Program
-    {
+    class Program { 
+
+        static string username;
+        static string password;
+
         static void Main(string[] args)
         {
-            bool isAdmin = false;
-            bool isRegistered = true;
-            string userName = "";
-            Console.WriteLine("Please enter your user name");
-            userName = Console.ReadLine();
-
-            if (isRegistered && userName != "" && userName.Equals("admin"))
-            {
-                Console.WriteLine("Hi there, registred user");
-                Console.WriteLine("Hi there " + userName);
-                Console.WriteLine("Hi there, Admin");
-                
-            }
-
-            if(isAdmin || isRegistered)
-            {
-                Console.WriteLine("You are loged in");
-            }
-
+            Register();
+            login();
             Console.Read();
-            
         }
 
-       
+       public static void Register()
+        {
+            Console.WriteLine("please enter your username");
+            username = Console.ReadLine();
+            Console.WriteLine("please enter your password");
+            password = Console.ReadLine();
+            Console.WriteLine("Registration complited");
+            Console.WriteLine("-------------------------------------------");
+
+        }
+
+        public static void login()
+        {
+            Console.WriteLine("please enter your username");
+            if(username == Console.ReadLine())
+            {
+                Console.WriteLine("please enter your password");
+                if(password == Console.ReadLine())
+                {
+                    Console.WriteLine("Login Succseflul");
+                }
+                else
+                {
+                    Console.WriteLine("Login failed, wrong password. Restart Program");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Login failed, wrong password. Restart Program");
+            }
+        }
 
 
     }
