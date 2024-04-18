bc78d88583086a055ba41864c14b406f743ba4a0
Aleksandar <60047712+Aleksandar-93@users.noreply.github.com>
2024-04-18 23:05:34 +0200

    loop pratice

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -4,21 +4,46 @@ using System.Reflection.Metadata;
 using System.Security.Cryptography.X509Certificates;
 using System.Threading.Channels;
 
-namespace BrakeContinu
+namespace LoopChallenge
 {
     class Program {
 
         
         static void Main(string[] args)
         {
-            for(int i = 0; i < 10;  i++)
+            string input = "0";
+            int count = 0;
+            int total = 0;
+            int currentNumber = 0;
+
+            while (input != "-1")
             {
-                if(i %2 == 0)
+                Console.WriteLine("Last number was {0}", currentNumber );
+                Console.WriteLine("Please enter the next score");
+                Console.WriteLine("Currnt amount of entires {0}", count);
+                Console.WriteLine("Please entere -1 once you are redy yo calculate the averge");
+
+                input = Console.ReadLine();
+                if (input.Equals("-1"))
                 {
-                    Console.WriteLine("Now comes an odd number");
+                    Console.WriteLine("-------------------------------------");
+                    double avrage = (double)total / (double)count;
+                    Console.WriteLine("the avrage score of your student is {0}", avrage);
+                }
+                if(int.TryParse(input, out currentNumber) && currentNumber >0 && currentNumber < 21 )
+                {
+                    total += currentNumber;
+                }
+                else
+                {
+                    if(!(input.Equals("-1")))
+                    {
+                        Console.WriteLine("Please enter a value betwen 1 and 20!");
+                    }
                     continue;
                 }
-                Console.WriteLine(i);
+
+                count++;
 
             }
 
